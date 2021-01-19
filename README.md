eea345fc73647488b2360af5271f0fca58617865
wilybear <samerj9712@gmail.com>
2021-01-20 00:54:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS193p_Assignment3_SetGame
+ CS193p Assignment3 SetGame
---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by 김현식 on 2021/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
